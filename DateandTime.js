a95b2398727dbc7221d187638710682577e272cb
amitojj <amitojj@gmail.com>
2023-11-27 15:47:17 +0530

    date and time

---
+++ DateandTime.js
@@ -0,0 +1,8 @@
+const mydate = new Date();
+console.log(mydate);
+console.log(mydate.getTime());
+console.log(mydate.getFullYear());
+console.log(mydate.getDay());
+console.log(mydate.getDate());
+console.log(mydate.getMonth());
+console.log(mydate.toLocaleDateString());
